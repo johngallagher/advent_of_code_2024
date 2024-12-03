943c8cb35852fbc3e3f79fc5473b65e441caa8c8
John Gallagher <john@synapticmishap.co.uk>
2024-12-03 21:35:38 +0000

    feat: only use do() portions

--- day_3/lib/scanner.rb
+++ day_3/lib/scanner.rb
@@ -1,9 +1,39 @@
 class Scanner
   def call(input)
     input
-      .scan(/mul\((\d+),(\d+)\)/)
-      # .tap { |a| throw a }
-      .map { |a, b| a.to_i * b.to_i }
-      .sum
+      .then(&split)
+      .fetch(:do)
+      .flat_map do |operation|
+        operation
+          .scan(/mul\((\d+),(\d+)\)/)
+          .map { |a, b| a.to_i * b.to_i }
+      end.sum
+  end
+
+  def split
+    lambda do |input|
+      current_operation = :do
+      do_operations = []
+      dont_operations = []
+      input
+        .split(%r{(don't\(\)|do\(\))})
+        .each do |value|
+          if value == "do()"
+            current_operation = :do
+          elsif value == "don't()"
+            current_operation = :dont
+          elsif current_operation == :do
+            do_operations << value
+          elsif current_operation == :dont
+            dont_operations << value
+          else
+            throw "Never get here"
+          end
+        end
+      {
+        do: do_operations,
+        dont: dont_operations
+      }
+    end
   end
 end
